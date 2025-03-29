f09b4a4231acc3fc9d41f042090ce277b2fb4e83
KykyryzoB <148477723+KykyryzoB@users.noreply.github.com>
2025-03-29 21:25:42 +0500

    Update idk.lua

--- idk.lua
+++ idk.lua
@@ -1,7 +1,7 @@
 local games = loadstring(game:HttpGet('https://raw.githubusercontent.com/KykyryzoB/who-2/main/a.lua'))()
 if games[game.PlaceId] then
   local getgenv = getgenv or getfenv
-  getgenv().VoxulLib = 'https://raw.githubusercontent.com/Voxul/VoxulHub/main/Orion.lua'
+  getgenv().VoxulLib = 'https://raw.githubusercontent.com/Articles-Hub/ROBLOXScript/refs/heads/main/Library/Orion/Source.lua'
   getgenv().VoxulWindowCONF = {Name = "Voxul Hub", HidePremium = false, SaveConfig = true, ConfigFolder = "Voxul_ORIONLIB", IntroEnabled = false, IntroText = "Voxul Hub", IntroIcon = "http://www.roblox.com/asset/?id=6035039429"}
   loadstring(game:HttpGetAsync(games[game.PlaceId]))() 
 else 
